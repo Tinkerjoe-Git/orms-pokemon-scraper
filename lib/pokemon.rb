a262ef2d7ab9fd626b7ac87efdcfc657be10afe0
Tinkergit <nicholas.stagno@gmail.com>
2021-03-04 14:48:30 -0500

    commit tree 2ed0d6b01cf2f0bf0c47f629b31b2a39e3fc7c30

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
 class Pokemon
+    attr_accessor :name, :type, :db, :hp
+
+    def initialize(attributes)
+        attributes.each do |key, value|
+            if key=="id"
+                self.class.attr_reader(key)
+            else
+                self.class.attr_accessor(key)
+            end
+            self.send("#{key}=", value)
+        end
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+          INSERT INTO pokemon (name, type) VALUES (?, ?);
+        SQL
+        
+        db.execute(sql, [name, type])
+    end
+
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        result = db.execute(sql, id)[0]
+        hash={}
+        hash[:id]=result[0]
+        hash[:name]=result[1]
+        hash[:type]=result[2]
+        Pokemon.new(hash)
+    end
+
+
+
 end
